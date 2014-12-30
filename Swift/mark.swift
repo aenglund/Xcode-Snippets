9b0d2bbc971be0330df3960c3d172ecfcc561496
Albin Englund <mail@albinenglund.com>
2014-12-30 11:10:43 +0100

    Added two basic and convenient swift code snippets

---
+++ Swift/mark.swift
@@ -0,0 +1,8 @@
+// Mark
+// Dividers and labels to organize your code into sections
+//
+// Platform: All
+// Language: Swift
+// Completion Scopes: Class Implementation
+
+// MARK: <#Section#>
---
+++ Swift/unittest.swift
@@ -0,0 +1,10 @@
+// Unit test
+// A unit test function template
+//
+// Platform: All
+// Language: Swift
+// Completion Scopes: Class Implementation
+
+func test<#Name#>() {
+    <#Statements#>
+}
