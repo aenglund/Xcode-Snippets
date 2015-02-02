39fa6f1ff4f5ec2e32d3c8e2026c50d5d25d6eeb
Albin Englund <albin.englund@tactel.se>
2015-02-02 13:41:52 +0100

    Added an xcode snippet for method swizzling

---
+++ Obj-C/swizzle.m
@@ -0,0 +1,53 @@
+// swizzle
+// Method swizzling, useful when debugging framwork behaviours
+//
+// Platform: All
+// Language: Objective-C
+// Completion Scope: Top level
+
+#import <objc/runtime.h>
+
+@implementation <#Class#>(Swizzle)
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+
+        SEL originalSelector = @selector(<#method#>);
+        SEL swizzledSelector = @selector(xxx_<#method#>:);
+
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+
+        // When swizzling a class method, use the following:
+        // Class class = object_getClass((id)self);
+        // ...
+        // Method originalMethod = class_getClassMethod(class, originalSelector);
+        // Method swizzledMethod = class_getClassMethod(class, swizzledSelector);
+
+        BOOL didAddMethod =
+        class_addMethod(class,
+                        originalSelector,
+                        method_getImplementation(swizzledMethod),
+                        method_getTypeEncoding(swizzledMethod));
+
+        if (didAddMethod) {
+            class_replaceMethod(class,
+                                swizzledSelector,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod));
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
+}
+
+#pragma mark - Method Swizzling
+
+- (void)xxx_<#method#> {
+    [self xxx_<#method#>];
+}
+
+@end
+
